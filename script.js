6d661cbf2aab594273f45598e25b9c7bec134e98
Lakshya <lakshyarajputr@gmail.com>
2026-02-19 14:17:04 +0530

    add eventlistener to button

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
 const disp = document.getElementById("w1");
 const inputArea = document.getElementById("para");
+const btn = document.getElementById("btn");
+btn.addEventListener("click", startReader);
 
 function startReader() {
-    let text = inputArea.value;
-    if (!text.trim()) {
-        alert("Please enter some text first.");
-        return;
-    }
-    readWords(text);
+  let text = inputArea.value;
+  if (!text.trim()) {
+    alert("Please enter some text first.");
+    return;
+  }
+  readWords(text);
 }
 
-function parseWord(word){
-    mid = Math.floor((word.length / 2 ));
-     if (word.length === 0) return "";
-     if (word.length === 1) return `<span id="span">${word}</span>`;
-    let outStr = word.slice(0,mid) + `<span id="focus">${word[mid]}</span>` + word.slice(mid+1);
+function parseWord(word) {
+  mid = Math.floor(word.length / 2);
+  if (word.length === 0) return "";
+  if (word.length === 1) return `<span id="span">${word}</span>`;
+  let outStr =
+    word.slice(0, mid) +
+    `<span id="focus">${word[mid]}</span>` +
+    word.slice(mid + 1);
 
-    return outStr;
+  return outStr;
 }
 
-async function readWords(str){
-    let words = str.split(/\s+/);
-    console.log(words);
-    
-    for(let i of words){
-       disp.innerHTML = parseWord(i);
+async function readWords(str) {
+  let words = str.split(/\s+/);
+  console.log(words);
 
-              let delay = 200;
-              if (i.endsWith('.') || i.endsWith(',')) delay = 300;
+  for (let i of words) {
+    disp.innerHTML = parseWord(i);
 
-              await sleep(delay);
-    }
+    let delay = 200;
+    if (i.endsWith(".") || i.endsWith(",")) delay = 300;
+
+    await sleep(delay);
+  }
 }
-
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
